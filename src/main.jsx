4c1a41c3e613b9850d0393619b7c764436fbcb2c
Hamood88 <hamood.fursan@hotmail.com>
2026-01-16 21:15:08 -0500

    fix: correct main router setup - remove duplicate routing conflict

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import SplitAuthPage from "./pages/SplitAuthPage";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SplitAuthPage />} />
-        <Route path="/auth" element={<SplitAuthPage />} />
-        <Route path="/dashboard/*" element={<App />} />
-        <Route path="/*" element={<SplitAuthPage />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
